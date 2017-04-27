38c901339b573a100262ff9bedec765dce8dd00a
Warren Chan <warrenchan@warren-2.local>
2017-04-27 09:29:34 -0700

    initial commit

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -5,11 +5,11 @@ export const SORT_NEWEST = 'SORT_NEWEST';
 
 export const SORT_POPULAR = 'SORT_POPULAR';
 
-export const upVote = (id) => ({
+export const upVote = id => ({
      type: UP_VOTE, id });
 
-export const sortNewest = (id) => ({
-    type: SORT_NEWEST, id });
+export const sortNewest = posts => ({
+    type: SORT_NEWEST, posts });
 
-export const sortPopular = (votes) => ({
-    type: SORT_POPULAR, votes });
+export const sortPopular = posts => ({
+    type: SORT_POPULAR, posts });
